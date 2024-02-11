0321f0c391db1f48caed03df51cba0a31db093be
Abderrahman Youabd <youabd2000@gmail.com>
2024-02-11 18:29:55 +0100

    initial commit

---
+++ src/redux/Auth/Reducer.js
@@ -0,0 +1,63 @@
+import {
+  REGISTER_REQUEST,
+  REGISTER_SUCCESS,
+  REGISTER_FAILURE,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  GET_USER_REQUEST,
+  GET_USER_SUCCESS,
+  GET_USER_FAILURE,
+  LOGOUT,
+} from "./ActionTypes";
+
+const initialState = {
+  user: null,
+  loading: false,
+  error: null,
+  jwt: null,
+
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case REGISTER_REQUEST:
+    case LOGIN_REQUEST:
+      case GET_USER_REQUEST:
+      return { ...state, loading: true, error: null };
+
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
+      return { ...state, loading: false, jwt: action.payload.jwt };
+      case GET_USER_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          user: action.payload,
+         
+        };
+
+
+    
+  
+    
+    
+
+    case GET_USER_FAILURE:
+    case REGISTER_FAILURE:
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+        fetchingUser: false,
+      };
+    case LOGOUT:
+      localStorage.removeItem("jwt");
+      return { ...state, jwt: null, user: null };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
